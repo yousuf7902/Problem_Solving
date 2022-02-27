9448fe8ec2cdb6942713f011968aaa5752005899
Yousuf Hassan <mr.ahmedperfect80@gmail.com>
2022-02-27 20:09:44 +0600

    All codes are uploaded

--- Codeforces/CF_451A.cpp
+++ Codeforces/CF_451A.cpp
@@ -2,23 +2,13 @@
 using namespace std;
 
 int main(){
-    int n, m,count=0;
-    cin >> n>>m;
-    int sum;
-    if(n==1||m==1){
-        sum = n * m;
-    }
-    else{
-        sum = n + m;
-    }
-    while(sum>0){
+    int n, m, count = 0;
+    cin >> n >> m;
+
+    while(n!=0 && m!=0){
         count++;
-        if(sum==1){
-            sum = sum - 1;
-        }
-        else{
-            sum = sum - 2;
-        }
+        n--;
+        m--;
     }
     if(count%2==0){
         cout << "Malvika" << endl;
