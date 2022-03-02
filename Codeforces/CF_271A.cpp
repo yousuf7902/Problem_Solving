fa637da83d3fed09af751e82a461fb6f8dfb0295
Yousuf Hassan <mr.ahmedperfect80@gmail.com>
2022-03-02 22:13:20 +0600

    All codes are uploaded

--- Codeforces/CF_271A.cpp
+++ Codeforces/CF_271A.cpp
@@ -1,13 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int y;
+int main()
+{
+    int y, digit;
     cin >> y;
     
     while(1){
-        
+        y++;
+        int a = y % 10;
+        int b = (y / 10) % 10;
+        int c = (y / 100) % 10;
+        int d = (y / 1000) % 10;
+        if(a!=b && a!=c && a!=d && b!=c &&b!=d &&c!=d){
+            cout << y << endl;
+            return 0;
+        }
     }
-
     return 0;
 }
--- Codeforces/CF_337A.cpp
+++ Codeforces/CF_337A.cpp
@@ -1,23 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n, m,minNum=0,sub;
+int main()
+{
+    int n, m, temp, minNum = INT_MAX, sub;
     cin >> n >> m;
 
     int arr[m];
-    for (int i = 0; i < m;i++){
+    for (int i = 0; i < m; i++)
+    {
         cin >> arr[i];
     }
-    for (int i = 0; i < n;i++){
-        for (int j = i + 1; j < n;j++){
-            sub = arr[i] - arr[j];
-            if(sub>0){
+    for (int i = 0; i < m - 1; i++)
+    {
+        for (int j = i + 1; j < m; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < m - 1; i++)
+    {
+        for (int j = i + 1; j < m; j++)
+        {
+            if (arr[i] != arr[j])
+            {
+                sub = arr[i] - arr[j];
                 minNum = min(minNum, sub);
             }
         }
     }
-    cout << minNum<< endl;
-    
+    cout << minNum << endl;
+
     return 0;
 }
