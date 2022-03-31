cfb819460ef7d5a7fa58bb34d07bad3efe4c0df7
Yousuf Hassan <mr.ahmedperfect80@gmail.com>
2022-03-31 15:06:19 +0600

    All codes are up to date

---
+++ CodeChef/INCRIQ.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int x;
+    cin >> x;
+
+    if (x + 7 > 170)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
