078d8ac1a35ce32bea93956fcfd175f0ecb22ac0
Yousuf Hassan <mr.ahmedperfect80@gmail.com>
2022-05-15 00:23:26 +0600

    All codes are uploaded

--- Codeforces/CF_337A.cpp
+++ Codeforces/CF_337A.cpp
@@ -1,40 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
-    int n, m, temp, minNum = INT_MAX, sub;
+    int n, m;
     cin >> n >> m;
+ 
+    int arr[m],minNum=INT_MAX,diff;
 
-    int arr[m];
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m;i++){
         cin >> arr[i];
     }
-    for (int i = 0; i < m - 1; i++)
-    {
-        for (int j = i + 1; j < m; j++)
-        {
-            if (arr[i] < arr[j])
-            {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < m - 1; i++)
-    {
-        for (int j = i + 1; j < m; j++)
-        {
-            if (arr[i] != arr[j])
-            {
-                sub = arr[i] - arr[j];
-                minNum = min(minNum, sub);
-            }
-        }
+    sort(arr, arr + m);
+    minNum = arr[n-1] - arr[0];
+    for (int i = n; i <= m;i++){
+        diff = abs(arr[i - 1] - arr[i - n]);
+        minNum = min(minNum, diff);
     }
     cout << minNum << endl;
-
     return 0;
 }
