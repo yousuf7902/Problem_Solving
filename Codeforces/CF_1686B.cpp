43b7e36993a8ca9947e4005fd9ce2bfc2a012a0b
Yousuf Hassan <mr.ahmedperfect80@gmail.com>
2022-05-28 22:52:29 +0600

    All codes are uploaded

--- Codeforces/CF_1686B.cpp
+++ Codeforces/CF_1686B.cpp
@@ -1,73 +1,65 @@
-// Problem: B. Odd Subarrays
-// Contest: Codeforces - Codeforces Round #794 (Div. 2)
-// URL: https://codeforces.com/contest/1686/problem/B
-// Memory Limit: 256 MB
-// Time Limit: 1000 ms
-
-//~"~"~"~"~"~"~"~"~"~"~"~"~"~"~ ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥ ~"~"~"~"~"~"~"~"~"~"~"~"~"~"~//  
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
-// ##                                                                         ## //
-// ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
-// ##                                                                         ## //
-// ##                        CF Handle : Yousuf7902                           ## //
-// ##                                                                         ## //
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
-
-#include <bits/stdc++.h>
-using namespace std;
-
-//===============================================================================
-//
-#define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
-#define         vc vector
-#define         pair pair<int, int>
-#define         st set<int>
-#define         ll long long
-#define         eb emplace_back
-#define         sort(v) sort(v.begin(),v.end())
-#define         sortrev(v) sort(v.rbegin(),v.rend())
-#define		   	py				cout << "Yes\n";
-#define		    pn				cout << "No\n";
-#define 	    endl            "\n"
-#define         rn return 0;
-//
-//===============================================================================
-
-int main(){
-    FAST
-	
-	int t;
-	cin>>t;
-	
-	while(t--){
-		int n,mid,count=0;
-		cin>>n;
-		
-		int arr[n+1];
-		
-		for(int i=1;i<=n;i++){
-			cin>>arr[i];
-		}
-		
-		
-		
-		if(n%2==0) mid=n/2;
-		else mid=(n/2)+1;
-		
-		for(int i=1;i<mid;i++){
-			if(arr[i]> arr[i+1] && i+1!=mid){
-				count++;
-			}
-		}
-		
-		for(int i=mid;i<n;i++){
-			if(arr[i]>arr[i+1]){
-				count++;
-			}
-		}
-		cout<<count<<endl;
-	}
-
-    rn;
-}
+    // Problem: B. Odd Subarrays
+    // Contest: Codeforces - Codeforces Round #794 (Div. 2)
+    // URL: https://codeforces.com/contest/1686/problem/B
+    // Memory Limit: 256 MB
+    // Time Limit: 1000 ms
+     
+    //~"~"~"~"~"~"~"~"~"~"~"~"~"~"~ ♥ B I S M I L L A H I R  R A H M A N I R  R A H I M ♥ ~"~"~"~"~"~"~"~"~"~"~"~"~"~"~//  
+     
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+    // ##                                                                         ## //
+    // ##          Yousuf Hassan || CSE,IUBAT || yousufhassan04@gmail.com         ## //
+    // ##                                                                         ## //
+    // ##                        CF Handle : Yousuf7902                           ## //
+    // ##                                                                         ## //
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+        
+        #include <bits/stdc++.h>
+        using namespace std;
+     
+        //===============================================================================
+        //
+        #define         FAST ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+        #define         vc vector
+        #define         pair pair<int, int>
+        #define         st set<int>
+        #define         ll long long
+        #define         eb emplace_back
+        #define         sort(v) sort(v.begin(),v.end())
+        #define         sortrev(v) sort(v.rbegin(),v.rend())
+        #define			py		cout << "Yes\n";
+        #define			pn		cout << "No\n";
+        #define			endl     "\n"
+        #define         rn return 0;
+        //
+        //===============================================================================
+     
+        int main(){
+            FAST
+     
+        	int t;
+        	cin>>t;
+     
+        	while(t--){
+        		int n,mid,count=0,pos=1;
+        		cin>>n;
+     
+        		int arr[n+1];
+     
+        		for(int i=1;i<=n;i++){
+        			cin>>arr[i];
+        		}
+     
+        		for(int i=1;i<n;i++){
+                    if(arr[i]>arr[i+1]){
+                        count++;
+                        i++;
+                    }
+        		}
+     
+        		cout<<count<<endl;
+     
+        	}
+     
+            rn;
+        }
